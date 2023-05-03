b0eca8975109d874c6e22e6c4edfff766ee3ccc5
PedroSimbera <pedrosimbera@hotmail.com>
2023-05-02 21:51:46 -0300

    add arquivo de tratamento de dados

---
+++ TratamentoDados.py
@@ -0,0 +1,6 @@
+#Tratamentos input e output
+#Por exemplo, é necessário o tratamento dos dados quando se pede a entrada de string e logo em seguida pede uma soma
+#Neste caso uma string não será somada ao número
+
+#a = input("Digite um número: ")
+#print (a + 2.5)
