ad7635b98d57b9ebf4fd3d122fd709f02f707c68
PedroSimbera <pedrosimbera@hotmail.com>
2023-05-02 21:47:40 -0300

    conteúdo de saida de dados

---
+++ SaidaDados.py
@@ -0,0 +1,14 @@
+#Saída de dados em Python
+# Toda saída de dados pelo programa ao usuário 
+# Termo print em python realiza a saída de determinado trecho em aspas, ou de varivável
+# A concatenação em python é por virgula, que de origem já adiciona um espaço entre texto pelo programador e variável.
+#O CTRL ; comenta o trecho selecionado, e ao apertar novamente ele retira de comentário.
+
+
+
+
+nome = input("Qual o seu nome: ")
+nascimento = input("Qual seu ano de nascimento? ")
+email = input("Qual seu email?")
+
+print("Seu nome",nome,",seu ano de nascimento é:",nascimento)
