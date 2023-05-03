52e241905db23d7f0803363c716bbe7f9c93f00f
PedroSimbera <pedrosimbera@hotmail.com>
2023-05-03 17:52:07 -0300

    sobre operadores

---
+++ OperadoresLógicos.py
@@ -0,0 +1,12 @@
+# 1a Categoria: Aritméticos
+# + - / *
+# 2a Categoria: Relacionais
+# > < >= <= == != === (último é o idêntico)
+# Maior e menor tem como nome técnico Chevron que traduzido para matemática é maior que e menor que.
+# 3a Categoria: Lógicos
+# ! (não) && (E) || (ou)
+
+#Outros operadores:
+# potência ** (2**3)=8 2*2*2
+# raiz sqrt ()
+# modulo (resto da divisao - 18 % 4 = 2)
