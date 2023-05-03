c7bdbd909c759bc798eda9591a1bfce959532f21
PedroSimbera <pedrosimbera@hotmail.com>
2023-05-02 21:37:49 -0300

    Sobre entrada de dados em python

--- EntradaDados.py
+++ EntradaDados.py
@@ -1,1 +1,7 @@
 #Entrada de dados em Python
+#Toda entrada pelo usuário armazenada em variável
+# Termo input em python realiza o armazenamento da entrada em uma variável
+
+nome = input("Qual o seu nome: ")
+nascimento = input("Qual seu ano de nascimento? ")
+email = input("Qual seu email?")
